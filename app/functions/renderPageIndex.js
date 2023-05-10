62a42a9c838c1445e27d0187f57a4db23abd104e
benwatsonuk <hello@benwatson.eu>
2023-05-10 11:05:45 +0100

    Updated to fix page lists

--- app/functions/renderPageIndex.js
+++ app/functions/renderPageIndex.js
@@ -2,8 +2,8 @@ const nunjucks = require('nunjucks')
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function (pages) {
+module.exports = function (pages, prefix) {
     const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '../templates/')))
-    const output = env.render('pageFlow.njk', { pages: pages })
+    const output = env.render('pageFlow.njk', { pages: pages, prefix: prefix })
     return output
 }
